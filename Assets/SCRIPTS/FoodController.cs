9055c69e0d4e8c62b5dff5b085f39144dd3faf56
narkos <narkos.newb@gmail.com>
2019-01-06 15:42:05 +0100

    initial commit. basic gameplay

---
+++ Assets/SCRIPTS/FoodController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodController : MonoBehaviour
+{
+    public GameObject foodPrefab;
+    public int halfWidth = 15;
+    public int halfHeight = 15;
+
+    private bool shouldSpawnFood = true;
+    private int amount = 5;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (shouldSpawnFood)
+        {
+            Spawn();
+            shouldSpawnFood = false;
+        }
+    }
+
+    public void SpawnFood()
+    {
+        shouldSpawnFood = true;
+    }
+
+    private void Spawn()
+    {
+        if (foodPrefab != null)
+        {
+            int missing = amount - transform.childCount;
+            for (int i = 0; i < missing; i++)
+            {
+
+                //print(x + " " + y);
+                //print(pos);
+                Instantiate(foodPrefab, FindSpawnPosition(), Quaternion.identity, transform);
+
+            }
+        }
+    }
+
+    private Vector3 FindSpawnPosition()
+    {
+        Vector3 pos = Vector3.zero;
+        bool foundPosition = false;
+        while (!foundPosition)
+        {
+            int x = (int)Random.Range(0, Screen.width);
+            int y = (int)Random.Range(0, Screen.height);
+            pos = Camera.main.ScreenToWorldPoint(new Vector3(x, y, 150));
+            pos.x = Mathf.Floor(pos.x);
+            pos.z = Mathf.Floor(pos.z);
+            
+            Collider[] hits = Physics.OverlapSphere(pos, 0.4f);
+            if(hits.Length == 0)
+            {
+                foundPosition = true;
+            } else {
+                print("collided with a thing");
+            }
+        }
+        return pos;
+    }
+}
---
+++ Assets/SCRIPTS/WormController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+    public GameObject bodyPrefab;
+    public float interval = 0.5f;
+    public FoodController foodController;
+    private Vector3 currentDirection = Vector3.right;
+    private IEnumerator moveRoutine;
+    private bool move = false;
+    private List<GameObject> bodyParts = new List<GameObject>();
+    private int length = 1;
+    private bool didEat = false;
+    private Vector3 spawnPosition;
+
+    void Start()
+    {
+        if (foodController != null)
+        {
+
+        }
+        move = true;
+        moveRoutine = MoveTimer();
+        StartCoroutine(moveRoutine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.0f)
+            {
+                currentDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+            }
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.0f)
+            {
+                currentDirection = new Vector3(0, 0, Input.GetAxisRaw("Vertical"));
+            }
+        }
+        if (move)
+        {
+            HandleBodyPart(spawnPosition);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Food")
+        {
+            length++;
+            Destroy(other.gameObject);
+            foodController.SpawnFood();
+        }
+        //else if (other.gameObject != bodyParts[0])
+        else
+        {
+            print(other.gameObject.name);
+            //bodyParts.Clear();
+            foreach(GameObject part in bodyParts) 
+            {
+                Destroy(part);
+            }
+            bodyParts.Clear();
+            length = 1;
+        }
+    }
+
+    private void HandleBodyPart(Vector3 position) 
+    {
+        if (bodyParts.Count < length)
+        {
+            bodyParts.Insert(0, (GameObject)Instantiate(bodyPrefab, position, Quaternion.identity));
+        }
+        else
+        {
+            bodyParts.Last().transform.position = position;
+            bodyParts.Insert(0, bodyParts.Last());
+            bodyParts.RemoveAt(bodyParts.Count - 1);
+        }
+        move = false;
+    }
+
+    IEnumerator MoveTimer()
+    {
+        yield return new WaitForSeconds(interval);
+        spawnPosition = transform.position;
+        transform.Translate(currentDirection);
+        move = true;
+        StartCoroutine(MoveTimer());
+    }
+}
