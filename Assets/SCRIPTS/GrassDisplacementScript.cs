864744ac031a2172f421f167a0ea27cce5a9aef4
narkos <narkos.newb@gmail.com>
2019-01-14 19:50:38 +0100

    grass shaders

---
+++ Assets/SCRIPTS/GrassDisplacementScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassDisplacementScript : MonoBehaviour
+{
+    [SerializeField]
+    private float radius;
+    [SerializeField]
+    private Material grassMaterial;
+
+    void Start()
+    {
+        if (grassMaterial != null)
+        {
+            //grassMaterial.SetFloat()
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grassMaterial != null)
+        {
+            grassMaterial.SetVector("_TargetPosition", transform.position);
+        }
+    }
+}
